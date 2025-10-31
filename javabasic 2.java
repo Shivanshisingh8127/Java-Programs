27ce6acb99ee01adac62f80aba78943ce72608a2
Shivanshisingh8127 <pransh8127@gmail.com>
2025-10-31 11:59:36 +0530

    Add files via upload

---
+++ javabasic 2.java
@@ -0,0 +1,10 @@
+
+//2nd code draw a pattern 
+public class javabasic {
+    public static void main(String args[]) {
+        System.out.println("* * * *");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.print("*\n");
+    }
+}
