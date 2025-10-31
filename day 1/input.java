85c707187e3b3c3187704e8a0026bffabcc6dd34
Shivanshisingh8127 <pransh8127@gmail.com>
2025-10-31 13:24:35 +0530

    Add files via upload

---
+++ day 1/input.java
@@ -0,0 +1,16 @@
+//input using operators sum substraction 
+import java.util.*;
+public class input {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+       /* 
+        float pi= sc.nextFloat();
+        int rad= sc.nextInt();
+        float area= pi*rad*rad;
+        System.out.print(area);
+        */
+       float a=sc.nextFloat();
+       double area =a*a;
+       System.out.println(" the area of squeare is=" +area);
+    }
+}
