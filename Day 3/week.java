74ce49ebae72e644c827154947270a6177e2c057
Shivanshisingh8127 <pransh8127@gmail.com>
2025-11-17 22:35:01 +0530

    Add files via upload

--- Day 3/week.java
+++ Day 3/week.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class  week {
     public static void main(String args[]){
-// program to find the weak name
+// program to find the weak name  using switch statement
        Scanner sc=new Scanner(System.in);
        System.out.println("enetr day number");
        int num=sc.nextInt();
