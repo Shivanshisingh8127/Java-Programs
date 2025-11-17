3f0076246a9c3e07a848ab07b4e2f81461d0d360
Shivanshisingh8127 <pransh8127@gmail.com>
2025-11-17 21:44:17 +0530

    Add files via upload

---
+++ Day 2/operation.java
@@ -0,0 +1,13 @@
+//operation perform 
+public class operation {
+    public static void main(String args[]){
+        int a= 54;
+        int b=23;
+        int c=45;
+        int sum =a+b;
+        int sub= sum-c;
+        System.out.println(sum);
+        System.out.println(sub);
+
+    }
+}
---
+++ Day 2/ques.java
@@ -0,0 +1,61 @@
+//question1 program of input 3 numbers and output average of 3 numbers
+/*
+import java.util.*;
+public class ques {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int num1= sc.nextInt();
+        float num2=sc.nextFloat();
+        long num3= sc.nextLong();
+        long average= (long)(num1+num2+num3)/3;
+        System.out.println(average);
+
+    }
+}
+*/
+// QUESTION2 
+/*
+import java.util.*;
+public class ques {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        float pen=3.0f;
+        float era=7.0f;
+        float sharpner=10.0f;
+        float total=(18*(pen+era+sharpner))/100;
+        int i=(int)total;
+        System.out.println(i);
+    }
+}
+*/
+
+
+/*ques3 
+import java.util.*;
+public class ques {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        byte b=4;
+        char c='a';
+        short s=512;
+        int i= 1000;
+        float f=3.14f;
+        double d=99.9954;
+        double result=(f*b)+(i%c)-(d*s);
+        System.out.print(result);
+    }
+}
+*/
+
+
+//ques4
+import java.util.*;
+public class ques {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int a =34;
+        float f=sc.nextFloat();
+        System.out.println(a>=f);
+    }
+}
+
